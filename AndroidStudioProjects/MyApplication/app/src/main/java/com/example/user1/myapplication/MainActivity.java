b02851c8f02f415f77f951f43931a78417fe6df5
gzn <ochimusha.jp@gmail.com>
2017-04-26 18:42:17 +0900

    Studio-making CALC-ver.up 1.0

--- AndroidStudioProjects/MyApplication/app/src/main/java/com/example/user1/myapplication/MainActivity.java
+++ AndroidStudioProjects/MyApplication/app/src/main/java/com/example/user1/myapplication/MainActivity.java
@@ -18,6 +18,110 @@ public class MainActivity extends AppCompatActivity {
     double result;
     boolean isRememberPushed;
 
+    View.OnClickListener buttonListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            txv.setText(etx.getText().toString());
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txv = (TextView)findViewById(R.id.textview1);
+        //txv.setText("=======TEXTVIEW=================");
+        String str = String.valueOf(33);
+        int i = Integer.parseInt("13");
+
+        int a = 1;
+        int b = 3;
+        float c =  (float) a/b;
+
+        etx = (EditText)findViewById(R.id.edittext);
+        //etx.setText("ここでテキストをエディット！");
+
+        //String getedit = etx.getText().toString();
+        //Log.e("EditOn: ",getedit);
+
+        //Toast tst = (Toast)
+        btn = (Button)findViewById(R.id.button);
+        //btn.setOnClickListener(new View.OnClickListener(){
+        //    public void onClick(View view){
+        //        txv.setText(etx.getText().toString());
+        //    }
+        //});
+        btn.setOnClickListener(buttonListener);
+
+        //上下のButtonキャスト・リスナー記述方法は同じ意味を持つ
+
+        findViewById(R.id.button_1).setOnClickListener(buttonNumListener);
+        findViewById(R.id.button_2).setOnClickListener(buttonNumListener);
+        findViewById(R.id.button_3).setOnClickListener(buttonNumListener);
+        findViewById(R.id.button_4).setOnClickListener(buttonNumListener);
+        findViewById(R.id.button_5).setOnClickListener(buttonNumListener);
+        findViewById(R.id.button_6).setOnClickListener(buttonNumListener);
+        findViewById(R.id.button_7).setOnClickListener(buttonNumListener);
+        findViewById(R.id.button_8).setOnClickListener(buttonNumListener);
+        findViewById(R.id.button_9).setOnClickListener(buttonNumListener);
+        findViewById(R.id.button_0).setOnClickListener(buttonNumListener);
+        findViewById(R.id.button_dot).setOnClickListener(buttonNumListener);
+
+        findViewById(R.id.button_divide).setOnClickListener(optButtonListener);
+        findViewById(R.id.button_multiply).setOnClickListener(optButtonListener);
+        findViewById(R.id.button_subtract).setOnClickListener(optButtonListener);
+        findViewById(R.id.button_add).setOnClickListener(optButtonListener);
+        findViewById(R.id.button_equal).setOnClickListener(optButtonListener);
+
+    }
+
+    View.OnClickListener optButtonListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            Button optbtn = (Button)view;
+            double value = Double.parseDouble(etx.getText().toString());
+            if(befkey == R.id.button_equal){
+                result = value;
+            }
+            else{
+                result = calc(befkey,result,value);
+                etx.setText(String.valueOf(result));
+            }
+
+            befkey = optbtn.getId();
+            txv.setText(optbtn.getText());
+            isRememberPushed = true;
+        }
+    };
+
+    View.OnClickListener clearButtonListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            befkey = R.id.button_equal;
+            result = 0;
+            isRememberPushed = false;
+
+            txv.setText("");
+            etx.setText("");
+        }
+    };
+
+    View.OnClickListener buttonNumListener = new View.OnClickListener(){
+        public void onClick(View view){
+            Button button = (Button)view;
+            //etx.append(button.getText());
+            //etx.setText(button.getText());
+
+            if(isRememberPushed == true){
+                etx.setText(button.getText());
+            }
+            else{
+                etx.append(button.getText());
+            }
+            isRememberPushed = false;
+        }
+    };
     double calc(int opt,double value1,double value2){
         switch(opt){
             case R.id.button_add:
@@ -33,86 +137,4 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        txv = (TextView)findViewById(R.id.textview1);
-        txv.setText("=======TEXTVIEW=================");
-        String str = String.valueOf(33);
-        int i = Integer.parseInt("13");
-
-        int a = 1;
-        int b = 3;
-        float c =  (float) a/b;
-
-        etx = (EditText)findViewById(R.id.edittext);
-        etx.setText("ここでテキストをエディット！");
-
-        String getedit = etx.getText().toString();
-        Log.e("EditOn: ",getedit);
-
-        //Toast tst = (Toast)
-        btn = (Button)findViewById(R.id.button);
-        btn.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View view){
-                txv.setText(etx.getText().toString());
-            }
-        });
-
-        //上下のButtonキャスト・リスナー記述方法は同じ意味を持つ
-
-        View.OnClickListener buttonNumListener = new View.OnClickListener(){
-            public void onClick(View view){
-                Button button = (Button)view;
-                //etx.append(button.getText());
-                //etx.setText(button.getText());
-
-                if(isRememberPushed == true){
-                    etx.setText(button.getText());
-                }
-                else{
-                    etx.append(button.getText());
-                }
-                isRememberPushed = false;
-            }
-        };
-
-        findViewById(R.id.button_1).setOnClickListener(buttonNumListener);
-        findViewById(R.id.button_2).setOnClickListener(buttonNumListener);
-        findViewById(R.id.button_3).setOnClickListener(buttonNumListener);
-        findViewById(R.id.button_4).setOnClickListener(buttonNumListener);
-        findViewById(R.id.button_5).setOnClickListener(buttonNumListener);
-        findViewById(R.id.button_6).setOnClickListener(buttonNumListener);
-        findViewById(R.id.button_7).setOnClickListener(buttonNumListener);
-        findViewById(R.id.button_8).setOnClickListener(buttonNumListener);
-        findViewById(R.id.button_9).setOnClickListener(buttonNumListener);
-        findViewById(R.id.button_0).setOnClickListener(buttonNumListener);
-        findViewById(R.id.button_dot).setOnClickListener(buttonNumListener);
-        findViewById(R.id.button_divide).setOnClickListener(buttonNumListener);
-        findViewById(R.id.button_multiply).setOnClickListener(buttonNumListener);
-        findViewById(R.id.button_subtract).setOnClickListener(buttonNumListener);
-        findViewById(R.id.button_add).setOnClickListener(buttonNumListener);
-        findViewById(R.id.button_equal).setOnClickListener(buttonNumListener);
-
-
-        View.OnClickListener optButtonListener = new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Button pushbtn = (Button)view;
-                double value = Double.parseDouble(etx.getText().toString());
-                if(befkey == R.id.button_equal){
-                    result = value;
-                }
-                else{
-                    result = calc(befkey,result,value);
-                }
-
-                befkey = pushbtn.getId();
-                txv.setText(pushbtn.getText());
-                isRememberPushed = true;
-            }
-        };
-    }
 }
