2d8ddce14b699c0fa21ca3a464f23760a574a3d6
gzn <ochimusha.jp@gmail.com>
2017-04-26 10:37:02 +0900

    Androdi studio git test

--- AndroidStudioProjects/MyApplication/app/src/main/java/com/example/user1/myapplication/MainActivity.java
+++ AndroidStudioProjects/MyApplication/app/src/main/java/com/example/user1/myapplication/MainActivity.java
@@ -2,6 +2,14 @@ package com.example.user1.myapplication;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+//import android.widget.TextView;
+//import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final TextView txv = (TextView)findViewById(R.id.textview1);
+        txv.setText("=======TEXTVIEW=================");
+        String str = String.valueOf(33);
+        int i = Integer.parseInt("13");
+
+        int a = 1;
+        int b = 3;
+        float c =  (float) a/b;
+
+         EditText etx = (EditText)findViewById(R.id.edittext);
+        etx.setText("ここでテキストをエディット！");
+
+        final String getedit = etx.getText().toString();
+        Log.e("EditOn: ",getedit);
+
+        //Toast tst = (Toast)
+        Button btn = (Button)findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                txv.setText(getedit);//etx.getText().toString());
+            }
+        });
     }
 }
